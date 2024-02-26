af1874e9894cabcce043340da9da01adfa544f90
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2024-02-26 14:15:38 -0600

    reorganizes + splitting

---
+++ 0_Initial_Exploration.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(tidyverse)
+
+anime <- read_csv("data/anime-filtered.csv")
+
+
+anime |>
+  filter(Score != 6.51) |>
+  ggplot(aes(Score))+
+  geom_bar()
+
+
+anime |>
+  filter(Type == "TV"|"ONA"|"OVA"|"Special")
+
+unique(anime$Type)
---
+++ 2_Initial_Setup.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(tidyverse)
+library(here)
+library(forcats)
+library(tidymodels)
+
+set.seed(22)
+
+# handle common conflicts
+tidymodels_prefer()
+
+clean <- read_csv("data/clean.csv")
+
+#split the data
+clean_split <- clean |>
+  initial_split(strata = score, prop = 0.7)
+
+clean_train <- clean_split |>
+  training()
+
+clean_test <- clean_split |>
+  testing()
+
+clean_folds <- vfold_cv(clean_train, v = 5, repeats = 3, strata = score)
+save(clean_train, clean_test, file = "results/clean_split.rda")
+save(clean_folds, file = "results/clean_folds.rda")
