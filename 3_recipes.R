66bc483c95a3a9165957af516bc5455a8f8d8337
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2024-03-07 16:29:19 -0600

    found tuning parameters for BT_base

--- 3_recipes.R
+++ 3_recipes.R
@@ -18,10 +18,10 @@ skimr::skim(clean_train)
 
 
 
-#baseline (kitchen sink)
+# Kitchen Sink Main:
 
 # Parametrics (lm)
-base_rec_param <-
+base_rec_main <-
   recipe(score ~ ., data = clean_train) |>
   step_rm(name) |>
   step_dummy(all_nominal_predictors()) |>
@@ -31,6 +31,8 @@ base_rec_param <-
 #  step_interact(~ episodes:duration) |>
 #  step_interact(~ ranking:episodes)
 
+
+# Kitchen Sink Trees:
 # trees non-param (trees)
 base_rec_trees <-
   recipe(score ~ ., data = clean_train) |>
@@ -45,8 +47,33 @@ base_rec_trees <-
 #prep(base_rec_param) |>
 #  bake(new_data = slice_head(clean_train, n = 5))
 
+# Main Complex:
+rec_main <-
+  recipe(score ~ ., data = clean_train) |>
+  step_rm(name) |>
+  step_dummy(all_nominal_predictors()) |>
+  step_center(all_numeric()) |>
+  step_scale(all_numeric()) |>
+  step_impute_mean(all_predictors()) |>
+  step_interact(~ episodes:duration) |>
+  step_interact(~ ranking:episodes)
+
+# Trees Complex:
+rec_trees <-
+  recipe(score ~ ., data = clean_train) |>
+  step_rm(name) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_center(all_numeric()) |>
+  step_scale(all_numeric()) |>
+  step_impute_mean(all_predictors())|>
+  step_interact(~ episodes:duration) |>
+  step_interact(~ ranking:episodes)
+
+
 # Save it all out
 save(base_rec_trees, file = here("results/base_rec_trees.rda"))
-save(base_rec_param, file = here("results/base_rec_param.rda"))
+save(base_rec_main, file = here("results/base_rec_main.rda"))
+save(rec_main, file = here("results/rec_main.rda"))
+save(rec_trees, file = here("results/rec_trees.rda"))
 
 
---
+++ 4_tune_bt_base.R
@@ -0,0 +1,109 @@
+library(ggplot2)
+library(tidyverse)
+library(here)
+library(tidymodels)
+library(doMC)
+library(doParallel)
+# load data + recipes
+load("results/clean_split.rda")
+load("results/clean_folds.rda")
+load("results/base_rec_main.rda")
+
+set.seed(234)
+num_cores <- parallel::detectCores(logical = TRUE)
+num_cores
+# 8
+num_cores <- registerDoMC(cores = 2)
+num_cores
+
+
+cl <- makePSOCKcluster(8)
+set.seed(44)
+registerDoParallel(cl)
+# model specifications ----
+
+library(xgboost)
+
+bt_spec <- boost_tree(mode = "regression",
+                       min_n = tune(),
+                       mtry = tune(),
+                       learn_rate = tune()) |>
+  set_engine("xgboost")
+
+# define workflows ----
+bt_base_workflow <- workflow() |>
+  add_model(bt_spec) |>
+  add_recipe(base_rec_main)
+
+# tune hyperparameters -----
+hardhat::extract_parameter_set_dials(bt_spec)
+
+bt_base_learn_rate <- c(0.09,0.11)
+bt_base_mtry <- c(1, 4)
+bt_base_min_n <- c(2, 7)
+bt_base_params <- extract_parameter_set_dials(bt_spec) %>%
+  update(learn_rate = learn_rate(bt_base_learn_rate)) |>
+  update(mtry = mtry(bt_base_mtry)) |>
+  update(min_n = mtry(bt_base_min_n))
+
+#write out grid
+bt_base_grid <- grid_regular(bt_base_params, levels = 5)
+bt_base_grid
+
+# fit workflows/models ----
+tuned_bt_base <- tune_grid(bt_base_workflow,
+                      clean_folds,
+                      grid = bt_base_grid,
+                      control = control_grid(save_workflow = TRUE))
+tuned_bt_base
+stopCluster(cl)
+
+
+# testing hyperparameters
+
+autoplot_bt_base <-autoplot(tuned_bt_base, metric = "rmse")
+save(autoplot_bt_base, file = here("exploration_results/autoplot_bt_base2.rda"))
+autoplot_bt_base
+
+bt_base_rmse <- as_workflow_set(
+  bt = tuned_bt_base
+)
+
+
+bt_base_rmse |>
+  collect_metrics() |>
+  filter(.metric == "rmse") |>
+  slice_min(mean, by = wflow_id) |>
+  arrange(mean) |>
+  select(`Model Type` = wflow_id,
+         `RMSE` = mean,
+         `Standard Error` = std_err,
+         `Number of Computations` = n) |>
+  knitr::kable(digits = c(NA, 2, 4, 0))
+
+
+#### Values:
+bt_base_learn_rate <- c(-10,-1)
+bt_base_mtry <- c(1, 4)
+bt_base_min_n <- c(1, 10)
+RMSE = 0.48
+STDERR = 0.0129
+
+bt_base_learn_rate <- c(-10,-1)
+bt_base_mtry <- c(1, 4)
+bt_base_min_n <- c(2, 40)
+RMSE = 0.48
+STDERR = 0.0112
+
+bt_base_learn_rate <- c(0.09,0.11)
+bt_base_mtry <- c(1, 4)
+bt_base_min_n <- c(1, 10)
+RMSE = 0.31
+STDERR = 0.0137
+#WINNER!!!
+
+bt_base_learn_rate <- c(0.09,0.11)
+bt_base_mtry <- c(1, 4)
+bt_base_min_n <- c(2, 7)
+RMSE = 0.32
+STDERR = 0.0123
